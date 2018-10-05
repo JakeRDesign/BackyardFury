8d245d853140ff2fbf90254028898083d20792ef
Cameron Brown <cameron@cmrn.io>
2018-10-05 11:25:17 +1000

    Fix warning about Projectile.body
    don't need to reference the RigidBody

--- BackyardFury/Assets/Scripts/Projectile.cs
+++ BackyardFury/Assets/Scripts/Projectile.cs
@@ -2,17 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody))]
 public class Projectile : MonoBehaviour
 {
     public delegate void ProjectileHitEvent();
     public ProjectileHitEvent onLand;
 
-    private Rigidbody body;
-
     void Awake()
     {
-        body = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
