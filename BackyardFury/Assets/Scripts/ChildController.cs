f2e75131a376347d3b570e9510bbda8ea4b4794d
Cameron Brown <cameron@cmrn.io>
2018-11-08 18:36:57 +1100

    Kids now move properly in relation to camera
    Pressing up will move the player in the direction that the camera is pointing, instead of just moving on the X axis

--- BackyardFury/Assets/Scripts/ChildController.cs
+++ BackyardFury/Assets/Scripts/ChildController.cs
@@ -16,15 +16,28 @@ public class ChildController : MonoBehaviour
 
     private GameObject holding = null;
     private Rigidbody body;
+    private Camera cam;
 
     private void Awake()
     {
         body = GetComponent<Rigidbody>();
+        cam = Camera.main;
     }
 
     private void FixedUpdate()
     {
         GamePadState state = GamePad.GetState((PlayerIndex)parentMode.GetParentController().playerIndex);
+
+        // get camera's forward and right directions so we move in the proper
+        // direction :)
+        Vector3 right = cam.transform.right;
+        Vector3 forward = cam.transform.forward;
+        // set Y values to 0 - we don't want any verticality
+        right.y = 0.0f;
+        forward.y = 0.0f;
+        // get back to a unit vector
+        right.Normalize();
+        forward.Normalize();
 
         float yRawInput = state.ThumbSticks.Left.Y;
         float xRawInput = state.ThumbSticks.Left.X;
@@ -35,11 +48,12 @@ public class ChildController : MonoBehaviour
         if (Mathf.Abs(Input.GetAxis("Vertical")) > 0)
             yRawInput = Input.GetAxis("Vertical");
 
-        float xMov = xRawInput * forwardDirection;
-        float yMov = yRawInput * forwardDirection;
+        // get movement vectors based on camera directions
+        Vector3 rightMovement = xRawInput * right;
+        Vector3 forwardMovement = yRawInput * forward;
 
         //Vector3 pos = transform.position;
-        Vector3 dif = new Vector3(xMov, 0.0f, yMov) * -moveSpeed * Time.fixedDeltaTime * 1000.0f;
+        Vector3 dif = (rightMovement + forwardMovement) * moveSpeed * Time.fixedDeltaTime * 1000.0f;
         //pos -= new Vector3(xMov, 0.0f, yMov) * moveSpeed * Time.fixedDeltaTime;
         //transform.position = pos;
         body.AddForce(dif);
@@ -47,7 +61,7 @@ public class ChildController : MonoBehaviour
         Vector3 vel = body.velocity;
         if (vel.sqrMagnitude > 1.0f)
         {
-            float ang = Mathf.Atan2(vel.x, vel.z) * Mathf.Rad2Deg - 90.0f;
+            float ang = Mathf.Atan2(vel.x, vel.z) * Mathf.Rad2Deg;
             transform.rotation = Quaternion.Euler(0.0f, ang, 0.0f);
         }
 
