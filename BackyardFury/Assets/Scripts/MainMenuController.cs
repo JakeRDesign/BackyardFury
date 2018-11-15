a316df0184d303e97002c0a5547e4fa28d239461
Cameron Brown <cameron@cmrn.io>
2018-11-15 15:47:17 +1100

    Added Bronwyn's menus!
    - Changed EditorBuildSettings with the new scenes with the correct build indices

--- BackyardFury/Assets/Scripts/MainMenuController.cs
+++ BackyardFury/Assets/Scripts/MainMenuController.cs
@@ -1,7 +1,8 @@
-﻿using System.Collections;
+﻿    using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class MainMenuController : MonoBehaviour {
 
@@ -13,11 +14,45 @@ public class MainMenuController : MonoBehaviour {
         Cursor.visible = true;
     }
 
+    // Loads game
     public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
 
+    // Loads Settings
+    public void Settings()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    // Volume control Slider
+    public Slider Volume;
+    public AudioSource Sound;
+    public void SoundSlider ()
+    {
+        Sound.volume = Volume.value;
+    }
+
+    // Load the Credits
+    public void Credits()
+    {
+        SceneManager.LoadScene(3);
+    }
+
+    // Adding mute
+    public void Mute()
+    {
+        AudioListener.pause = !AudioListener.pause;
+    }
+
+    // Back to main menu
+    public void Back()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    // Quits Game
     public void QuitGame()
     {
         Application.Quit();
