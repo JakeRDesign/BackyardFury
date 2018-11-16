3cf61fc4e2bda31156b70982c60127da5238058e
adamrabbitson <39174595+adamrabbitson@users.noreply.github.com>
2018-11-16 11:19:30 +1100

    Added Gamemodes menu with scripts to switch between Boxing Match Mode & Last Box Standing Mode.

---
+++ BackyardFury/Assets/Scripts/GamemodesMenuController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GamemodesMenuController : MonoBehaviour {
+
+	void Start () {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void LastBoxMode()
+    {
+        SceneManager.LoadScene(4);
+    }
+
+    public void BoxingMatchMode()
+    {
+        SceneManager.LoadScene(5);
+    }
+}
