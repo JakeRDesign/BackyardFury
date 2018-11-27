8337e7a8ee2e7892ad7e26a3488b91144bbca2cf
Jake Rowe <39175090+JakeRDesign@users.noreply.github.com>
2018-11-28 10:43:29 +1100

    Triggers. Street NPCs set up.

---
+++ BackyardFury/Assets/Scripts/AudioTriggers.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioTriggers : MonoBehaviour {
+
+	public AudioSource npc;
+	public bool playedonce;
+
+	void Start()
+	{
+		playedonce = false;
+	}
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.tag == "Projectile" && playedonce == false)
+			{
+				npc.Play();
+				playedonce = true;
+				Destroy(other, 1f);
+				Invoke ("Reset", 3f);
+			}
+			
+}
+
+	void Reset()
+	{
+		playedonce = false;
+	}
+}
