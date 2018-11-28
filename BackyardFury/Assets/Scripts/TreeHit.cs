adc5983958571c2ab718f86b34a6cc1e2446f45a
adamrabbitson <39174595+adamrabbitson@users.noreply.github.com>
2018-11-29 10:31:28 +1100

    Added in leaves falling (and hit sound) when hitting a tree.

---
+++ BackyardFury/Assets/Scripts/TreeHit.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeHit : MonoBehaviour {
+
+    public GameObject particle;
+
+    private void OnTriggerEnter(Collider other) {
+        if (other.tag == "Projectile") {
+            PlayParticle();
+        }
+    }
+
+    void PlayParticle() {
+        particle.SetActive(true);
+        StartCoroutine(waitThreeSeconds());
+    }
+
+    IEnumerator waitThreeSeconds() {
+        yield return new WaitForSeconds(3.0f);
+        particle.SetActive(false);
+    }
+
+}
